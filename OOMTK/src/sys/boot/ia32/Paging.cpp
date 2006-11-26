3f15d64bdb52fa5c52017b52f3e29fd642730ce9
brcha <brcha@arthur.local>
2006-11-26 13:23:45 +0000

    [oomtk @ svn-v2:11@edf8bf19-1323-0410-a13a-b46ca9a76415-trunk] [OOMTK @ Paging base abstract class]
    Original author: Filip Brcic <brcha@users.sourceforge.net>
    Date: 2006-11-22 18:26:04

---
+++ OOMTK/src/sys/boot/ia32/Paging.cpp
@@ -0,0 +1,24 @@
+INTERFACE:
+/*
+ *  Copyright (C) 2006 by Filip Brcic <brcha@users.sourceforge.net>
+ *
+ *  This file is part of OOMTK
+ */
+/** @file
+ * @brief Paging support
+ *
+ * @todo Is this file IA32 specific or generic???
+ */
+
+/**
+ * @brief Paging base class
+ *
+ * This is the base class for paging support. It has to be inherited and implemented
+ * for specific architecture and specific paging method.
+ */
+class Paging
+{
+};
+
+IMPLEMENTATION:
+
